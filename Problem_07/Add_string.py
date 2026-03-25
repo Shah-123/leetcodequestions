2af159c815c2b2c3034b62bdd108ce2fb78b16a0
Shah-123 <shahkarahmad342@gmail.com>
2026-03-25 22:55:56 +0500

    problem_07

---
+++ Problem_07/Add_string.py
@@ -0,0 +1,9 @@
+
+class Solution:
+    def addStrings(num1, num2):
+        
+        return str(int(num1) + int(num2))
+num1 = "11"
+num2 = "123"
+solution = Solution()
+print(solution.addStrings(num1, num2))
