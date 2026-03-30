96dfc2c48bc906952b87a61d9c7134b9bd2151d3
Shah-123 <shahkarahmad342@gmail.com>
2026-03-30 23:56:35 +0500

    Problem_09

---
+++ RomantoInteger -problem_09/romantoInt.py
@@ -0,0 +1,23 @@
+def romanToInt(s):
+    roman_dict = {
+        "I": 1, "V": 5, "X": 10,
+        "L": 50, "C": 100, "D": 500,
+        "M": 1000
+    }
+    total = 0
+    prev_value = 0
+    for ch in reversed(s):
+        value = roman_dict[ch]
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        prev_value = value
+    return total
+
+# examples
+print(romanToInt("III"))    # 3
+print(romanToInt("IV"))     # 4
+print(romanToInt("IX"))     # 9
+print(romanToInt("LVIII"))  # 58
+print(romanToInt("MCMXCIV"))# 1994
